e64c44b80a601cfad1ddfd04de5be3cf838d624b
Misael <misael.lima26@gmail.com>
2025-11-18 17:59:39 -0300

    VERSAO 2 COM O BANCO DE DADOS ONLINE 2

--- app/src/main/java/com/example/gestaofeiracooperativa/DistribuirSobrasScreen.kt
+++ app/src/main/java/com/example/gestaofeiracooperativa/DistribuirSobrasScreen.kt
@@ -1,6 +1,5 @@
 package com.example.gestaofeiracooperativa
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
